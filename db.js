3000251001094cbb56707ff557cf262d0fad6e5c
bei <zhibei66@gmail.com>
2025-04-24 00:49:22 +0800

    更新数据库连接配置，修改用户、密码和数据库名称

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'root',
-    password: '123456',
-    database: 'art_data',
+    user: 'data',
+    password: '5z24cJEiMd34jAtt',
+    database: 'data',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
