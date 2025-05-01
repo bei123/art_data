388d02dd70218a171c8dd4d59793da2f2f445bfb
bei <zhibei66@gmail.com>
2025-05-01 20:45:14 +0800

    更新数据库配置，将默认数据库名称更改为'data'，并优化收藏功能中的用户身份验证逻辑，确保在添加、取消和获取收藏时使用统一的用户ID，提升代码的可维护性和安全性。

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const dbConfig = {
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'data',
     password: process.env.DB_PASSWORD || '5z24cJEiMd34jAtt',
-    database: process.env.DB_NAME || 'art_data',
+    database: process.env.DB_NAME || 'data',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
