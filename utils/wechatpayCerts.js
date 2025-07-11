e1d7116ecb61dcd7db4e05632566b56bc843af43
bei <zhibei66@gmail.com>
2025-07-11 19:40:17 +0800

    更新微信支付公钥ID的引用，改为使用新的公钥文件路径，确保证书管理逻辑的一致性和正确性。

--- utils/wechatpayCerts.js
+++ utils/wechatpayCerts.js
@@ -7,7 +7,7 @@ const keys = {
   // 平台证书序列号（推荐）
   // '18A6B90BBC01A2FD6B37DA8EDD7A62C48C52DDDD': fs.readFileSync(path.join(__dirname, '../platform_cert.pem'), 'utf8'),
   // 微信支付公钥ID（如你只用公钥文件）
-  // 'PUB_KEY_ID_0113606396022025041900441636000800': fs.readFileSync(path.join(__dirname, '../wxpay_pubkey.pem'), 'utf8'),
+   'PUB_KEY_ID_0113606396022025041900441636000800': fs.readFileSync(path.join(__dirname, '../pub_key.pem'), 'utf8'),
 };
 
 function getWechatpayPublicKey(serialOrKeyId) {
