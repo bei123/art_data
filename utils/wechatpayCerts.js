ba5c11894fcb742fa88b5caa13976f45879dab26
bei <zhibei66@gmail.com>
2025-07-11 18:50:20 +0800

    修正微信支付证书序列号格式，移除公钥ID前缀，确保与实际证书内容一致性。

--- utils/wechatpayCerts.js
+++ utils/wechatpayCerts.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 // 证书序列号与公钥内容的映射
 const certs = {
-  'PUB_KEY_ID_0113606396022025041900441636000800': fs.readFileSync(path.join(__dirname, '../pub_key.pem'), 'utf8')
+  '0113606396022025041900441636000800': fs.readFileSync(path.join(__dirname, '../pub_key.pem'), 'utf8')
 };
 
 function getWechatpayPublicKey(serialNo) {
