8f7aaefdb6f8777d1e38ce25cefdd986153e2e9a
bei <zhibei66@gmail.com>
2025-07-11 19:29:44 +0800

    重构微信支付证书管理逻辑，支持多种公钥/证书配置，提升代码可读性和灵活性。

--- utils/wechatpayCerts.js
+++ utils/wechatpayCerts.js
@@ -1,13 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
-// 证书序列号与公钥内容的映射
-const certs = {
-  '0113606396022025041900441636000800': fs.readFileSync(path.join(__dirname, '../pub_key.pem'), 'utf8')
+// 支持多种公钥/证书配置
+// key为微信支付平台证书序列号或公钥ID，value为公钥内容
+const keys = {
+  // 平台证书序列号（推荐）
+  // '18A6B90BBC01A2FD6B37DA8EDD7A62C48C52DDDD': fs.readFileSync(path.join(__dirname, '../platform_cert.pem'), 'utf8'),
+  // 微信支付公钥ID（如你只用公钥文件）
+  // 'PUB_KEY_ID_0113606396022025041900441636000800': fs.readFileSync(path.join(__dirname, '../wxpay_pubkey.pem'), 'utf8'),
 };
 
-function getWechatpayPublicKey(serialNo) {
-  return certs[serialNo];
+function getWechatpayPublicKey(serialOrKeyId) {
+  return keys[serialOrKeyId];
 }
 
 module.exports = { getWechatpayPublicKey }; 
